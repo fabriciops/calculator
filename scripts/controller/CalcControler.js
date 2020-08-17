c96f8c829515d808eba88b7f1f78669e477431f7
fabriciopps19@gmail.com <fabriciopps19@gmail.com>
2020-08-16 23:51:44 -0300

    upload diretórios

---
+++ scripts/controller/CalcControler.js
@@ -0,0 +1,44 @@
+class CalcController{
+
+    constructor(){
+        
+        this._displayCalc = "0";
+        this._currentDate;
+        this.initialize;
+        this.initialize();
+    }
+
+    initialize(){
+
+        let displayCalcEl = document.querySelector("#display");
+        let dateEl = document.querySelector("#data");
+        let timeEl = document.querySelector("#hora");
+
+        displayCalcEl.innerHTML ="456";
+        dateEl.innerHTML ="4894";
+        timeEl.innerHTML ="4894";
+    }
+
+    get displayCalc(){
+
+        return this._displayCalc;
+    }
+
+    set displayCalc(valor){
+
+        this._displayCalc = valor;
+
+    }
+    
+    get currentDate(){
+
+        return this._currentDate;
+    }
+
+    set currentDate(valor){
+
+        this._currentDate = valor;
+
+    }
+
+}
