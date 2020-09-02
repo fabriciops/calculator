3158e4aafe2238d4752ebcf3b72d1f1e8ab1b259
Fabrício Pinheiro dos Santos <fabriciopps19@gmail.com>
2020-09-02 01:18:06 -0300

    mudanças

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -1,2 +1,1 @@
-let calculator = new CalcController;
-calculator.initialize;
+window.calculator = new CalcController;
--- scripts/controller/CalcControler.js
+++ scripts/controller/CalcControler.js
@@ -2,42 +2,80 @@ class CalcController{
 
     constructor(){
         
-        this._displayCalc = "0";
-        this._currentDate;
-        this.initialize;
+        this._locale = "pt-BR";
+        this._displayCalcEl = document.querySelector("#display");
+        this._dateEl = document.querySelector("#date");
+        this._timeEl = document.querySelector("#time");
         this.initialize();
+        
+    
     }
+
+    setDisplayDateTime();
 
     initialize(){
 
-        let displayCalcEl = document.querySelector("#display");
-        let dateEl = document.querySelector("#data");
-        let timeEl = document.querySelector("#hora");
+        let interval = setInterval(()=>{
 
-        displayCalcEl.innerHTML ="456";
-        dateEl.innerHTML ="4894";
-        timeEl.innerHTML ="4894";
+            setDisplayDateTime();
+
+        },1000);
+
+        setTimeout(()=>{
+
+            clearInterval(interval);
+
+        }, 1000);
+
+    }
+
+    setDisplayDateTime(){
+
+        this.displayDate = this.currentDate.toLocaleDateString(this._locale);
+        this.displayTimes = this.currentDate.toLocaleDateString(this._locale);
     }
 
     get displayCalc(){
 
-        return this._displayCalc;
+        return this._displayCalcEl.innerHTML;
     }
 
-    set displayCalc(valor){
+    set displayCalc(value){
 
-        this._displayCalc = valor;
+        this._displayCalcEl = value;
+
+    }
+
+    get displayTime(){
+
+        return this._timeEl.innerHTML;
+    }
+
+    set displayTime(value){
+
+        this._timeEl.innerHTML = value;
+
+    }
+
+    get displayDate(){
+
+        return this._dateEl.innerHTML;
+    }
+
+    set displayDate(value){
+
+        this._dateEl.innerHTML = value;
 
     }
     
     get currentDate(){
 
-        return this._currentDate;
+        return new Date();
     }
 
-    set currentDate(valor){
+    set currentDate(value){
 
-        this._currentDate = valor;
+        this._currentDate = value;
 
     }
 
